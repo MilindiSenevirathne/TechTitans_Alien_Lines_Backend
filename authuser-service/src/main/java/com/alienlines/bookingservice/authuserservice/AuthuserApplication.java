44d4cbf5848b47418f6162df12236d023e76aa24
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2023-08-17 03:11:13 +0530

    Initial commit

---
+++ authuser-service/src/main/java/com/alienlines/bookingservice/authuserservice/AuthuserApplication.java
@@ -0,0 +1,13 @@
+package com.alienlines.bookingservice.authuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthuserApplication.class, args);
+    }
+
+}
