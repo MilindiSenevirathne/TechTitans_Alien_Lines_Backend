c4ffb0ea141ce50f73a6cba37bb84e455b313aa3
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2023-08-19 02:49:28 +0530

    Add destination service module

---
+++ destination-service/src/main/java/com/alienlines/destinationservice/DestinationApplication.java
@@ -0,0 +1,13 @@
+package com.alienlines.destinationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DestinationApplication.class, args);
+    }
+
+}
