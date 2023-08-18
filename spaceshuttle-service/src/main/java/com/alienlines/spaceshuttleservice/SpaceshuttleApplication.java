8370697a54d696a1fe9e59a48b35fb628ce3dff8
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2023-08-19 03:06:33 +0530

    add spaceshuttle service

---
+++ spaceshuttle-service/src/main/java/com/alienlines/spaceshuttleservice/SpaceshuttleApplication.java
@@ -0,0 +1,12 @@
+package com.alienlines.spaceshuttleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshuttleApplication {
+
+    public static void main(String[] args) {SpringApplication.run(SpaceshuttleApplication.class, args);
+    }
+
+}
